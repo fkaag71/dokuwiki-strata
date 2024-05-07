5b1ccf784770423822a57eeb268566ad0e6200fa
FKaag <francois.kaag@cardynal.fr>
2024-05-07 09:30:47 +0200

    Support the hint in ref typing
    In the [ref::namespace] typing, the namespace hint was not taken into account.

--- types/ref.php
+++ types/ref.php
@@ -15,7 +15,7 @@ class plugin_strata_type_ref extends plugin_strata_type_page {
         parent::__construct();
     }
 
-    function render($mode, &$R, &$T, $value, $hint) {
+    function render($mode, &$R, &$T, $value, $hint='') {
         $heading = null;
 
         // only use heading if allowed by configuration
@@ -29,7 +29,7 @@ class plugin_strata_type_ref extends plugin_strata_type_page {
         // render internal link
         // (':' is prepended to make sure we use an absolute pagename,
         // internallink resolves page names, but the name is already resolved.)
-        $R->internallink(':'.$value, $heading);
+        $R->internallink($hint.':'.$value, $heading);
     }
 
     function getInfo() {
